158420d516247be72b3a11faa3b28a0f8a6e4780
ThiraTheNerd <thiragithinji@gmail.com>
2021-02-18 20:33:30 +0300

    add new changes to the js folder

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,32 @@ var number1 = parseInt(prompt("Enter first number:"));
 var number2 = parseInt(prompt("Enter second number:"));
 
 alert(subtract(number1,number2));
+
+
+var add = function(num1,num2){
+  return num1 + num2;
+}
+
+var num1 = parseInt(prompt("Enter num1:"));
+var num2 = parseInt(prompt("Enter Num2:"));
+
+alert(add(num1,num2));
+
+var divide = function(digit1,digit2){
+  return digit1/digit2;
+}
+
+digit1 = parseInt(prompt("Enter first digit:"));
+digit2 = parseInt(prompt("Enter second digit:"));
+
+alert(divide(digit1,digit2));
+
+var multiply = function(dig1,dig2){
+  return dig1 * dig2 ;
+
+}
+
+var dig1 = parseInt(prompt("Enter dig1"));
+var dig2 = parseInt(prompt("Enter dig2:"));
+
+alert(multiply(dig1,dig2));
