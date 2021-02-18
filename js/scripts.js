f8218ea67df3b55797fd5ba5cf91f688aa9c3c61
ThiraTheNerd <thiragithinji@gmail.com>
2021-02-18 20:14:03 +0300

    add all the current changes to the git repo#

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var subtract = function(number1, number2){
+  return number1 - number2;
+}
+var number1 = parseInt(prompt("Enter first number:"));
+var number2 = parseInt(prompt("Enter second number:"));
+
+alert(subtract(number1,number2));
